33cab474d99dd607b14c7565411ef018f9fc9f6c
Christoph Korn <christoph.korn@posteo.de>
2022-04-09 22:16:45 +0200

    Just merge after the fetch
    The merge still makes sihost.exe consume 100% CPU. But at least it is a bit faster than pull because we just fetched before.

--- AutoGitWatcher/AutoGitWatcher.cs
+++ AutoGitWatcher/AutoGitWatcher.cs
@@ -197,7 +197,7 @@ namespace AutoGitWatcher
                             {
                                 WorkingDirectory = directory,
                                 FileName = "git",
-                                Arguments = "pull",
+                                Arguments = "merge origin/master --ff-only",
                                 CreateNoWindow = true,
                             });
                             processPull?.WaitForExit();
